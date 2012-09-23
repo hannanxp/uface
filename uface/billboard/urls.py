e8cef418673dad2d149d75bb7ebf2182bfe94d5b
Hannan Gigih Prastawa <hannanxp@gmail.com>
2012-09-23 17:17:03 +0700

    get user id

--- uface/billboard/urls.py
+++ uface/billboard/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('billboard.views',
     url(r'^$', 'index'),
     url(r'^test/$', 'test'),
+    url(r'^init/$', 'init'),
     #url(r'^(?P<poll_id>\d+)/$', 'detail'),
     #url(r'^(?P<poll_id>\d+)/results/$', 'results'),
     #url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
--- uface/billboard/views.py
+++ uface/billboard/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.utils import simplejson
+from django.core import serializers
 
 def index(request):
     return HttpResponse('Hello Kitty')
@@ -9,3 +10,29 @@ def test(request):
     data = {'a': 1, 'b': 2, 'john' : 'done', 'jane' : 'doe'}
     ret = simplejson.dumps(data)
     return HttpResponse(ret, 'application/javascript')
+    
+# Initial ajax request
+def init(request):
+    
+    user = user_from_session_key(request.session.session_key)
+    data = {'id': user.id, 'username': user.username}
+    ret = simplejson.dumps(data)
+    
+    return HttpResponse(ret, 'application/javascript')
+    
+def user_from_session_key(session_key):
+    from django.conf import settings
+    from django.contrib.auth import SESSION_KEY, BACKEND_SESSION_KEY, load_backend
+    from django.contrib.auth.models import AnonymousUser
+
+    session_engine = __import__(settings.SESSION_ENGINE, {}, {}, [''])
+    session_wrapper = session_engine.SessionStore(session_key)
+    session = session_wrapper.load()
+    user_id = session.get(SESSION_KEY)
+    backend_id = session.get(BACKEND_SESSION_KEY)
+    if user_id and backend_id:
+        auth_backend = load_backend(backend_id)
+        user = auth_backend.get_user(user_id)
+        if user:
+            return user
+    return AnonymousUser()
