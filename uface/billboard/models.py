c3760c49c90ffc22d220c5c31f70ef7274c500aa
Hannan Gigih Prastawa <hannanxp@gmail.com>
2012-09-28 20:54:12 +0700

    adjust css body height

--- uface/billboard/models.py
+++ uface/billboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BillboardUserModuleBox(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    modname = models.CharField(max_length=200)
+    posx = models.FloatField()
+    posy = models.FloatField()
--- uface/static/js/billboard.js
+++ uface/static/js/billboard.js
@@ -1,19 +1,31 @@
 jQuery(function($){
     function _init() {
         _load();
-        var pos, x = 0, y = 300;
+        var pos,
+            x = 0,
+            y = 300,
+            seth = false;
+            h = y;
         $(".module").each(function(){
+            seth = true;
             x += 100;
             y += 30;
+            h += $(this).height() + 110;
+            console.log(h);
             $(this).dialog({
                 position: [x, y],
                 open: function(event, ui) { 
-                    //hide close button.
                     $(this).parent().children().children('.ui-dialog-titlebar-close').hide();
                 },
+                dragStop: function(event, ui) {
+                    _changePos($(this).attr('id'), ui.position);
+                }
 
             });
         });
+        if (seth) {
+            $("body").height(h);
+        }
         
     }
     
@@ -26,6 +38,10 @@ jQuery(function($){
             dataType: "json"
         });
 
+    }
+    
+    function _changePos(id, pos) {
+        console.log(id, pos);
     }
     
     _init();
