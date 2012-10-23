19ca6231ccf441c9f465260c4fbde988046f0a95
Hannan Gigih Prastawa <hannanxp@gmail.com>
2012-10-23 21:41:22 +0700

    remove unused url n views method

--- uface/billboard/urls.py
+++ uface/billboard/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('billboard.views',
     url(r'^$', 'index'),
-    url(r'^test/$', 'test'),
-    url(r'^load/$', 'load'),
-    url(r'^chpos/$', 'chpos'),
     url(r'^acceptmsg/$', 'acceptmsg'),
     url(r'^delmsg/$', 'delmsg'),
     url(r'^saveapps/$', 'saveapps'),
--- uface/billboard/views.py
+++ uface/billboard/views.py
@@ -3,30 +3,13 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from django.core import serializers
 from django.core.exceptions import ObjectDoesNotExist
-from billboard.models import BillboardUserModuleBox
-from billboard.models import BbApps
 from postman.models import Message
 try:
     from django.utils.timezone import now   # Django 1.4 aware datetimes
 except ImportError:
     from datetime import datetime
     now = datetime.now
-
-def _get_messages(user):
-    msgs_i = []
-    msgs_p = []
-    msgs_u = []
-    messages = Message.objects.filter(recipient=user)
-    for msg in messages:
-        if msg.category == 'i':
-            msgs_i.append({'id': msg.id,'s': msg.subject,'b': msg.body,'a': msg.recipient_archived})
-        elif msg.category == 'p':
-            msgs_p.append({'id': msg.id,'s': msg.subject,'b': msg.body,'a': msg.recipient_archived})
-        elif msg.category == 'u':
-            msgs_u.append({'id': msg.id,'s': msg.subject,'b': msg.body,'a': msg.recipient_archived})
-
-    data = {'i': msgs_i, 'p': msgs_p, 'u': msgs_u}
-    return data
+from billboard.models import BbApps
 
 def _archive_msg(user, msg_id):
     msg = Message.objects.get(pk=msg_id)
@@ -58,41 +41,8 @@ def _delete_msg(user, msg_id):
 
 def index(request):
     user = request.user
-    data = _get_messages(user)
-    ret = simplejson.dumps(data)
-    return HttpResponse(ret, 'application/javascript')
-    
-def test(request):
-    data = {'a': 1, 'b': 2, 'john' : 'done', 'jane' : 'doe'}
-    ret = simplejson.dumps(data)
-    return HttpResponse(ret, 'application/javascript')
-    
-# Initial ajax request
-def load(request):
-    
-    user = request.user
-    jsbox = []
-    boxes = BillboardUserModuleBox.objects.filter(user=user)
-    for box in boxes:
-        jsbox.append({'modname': box.modname, 'posx': box.posx, 'posy': box.posy})
-
-    messages = _get_messages(user);
-    data = {'jsbox': jsbox, 'messages': messages}
-    ret = simplejson.dumps(data)
-    
-    return HttpResponse(ret, 'application/javascript')
-    
-# Change Box Position
-def chpos(request):
-    user = request.user
-    
-    box = get_or_create_userbox(user, request.POST['modname'])
-    box.posx = request.POST['posx']
-    box.posy = request.POST['posy']
-    box.save()
     data = {}
     ret = simplejson.dumps(data)
-    
     return HttpResponse(ret, 'application/javascript')
     
 # Change Box Position
@@ -114,7 +64,6 @@ def saveapps(request):
     
     return HttpResponse(ret, 'application/javascript')
     
-    
 # Mark message to archived by reader
 def acceptmsg(request):
     user = request.user
@@ -134,31 +83,6 @@ def delmsg(request):
     data = result
     ret = simplejson.dumps(data)
     return HttpResponse(ret, 'application/javascript')
-    
-def user_from_session_key(session_key):
-    from django.conf import settings
-    from django.contrib.auth import SESSION_KEY, BACKEND_SESSION_KEY, load_backend
-    from django.contrib.auth.models import AnonymousUser
-
-    session_engine = __import__(settings.SESSION_ENGINE, {}, {}, [''])
-    session_wrapper = session_engine.SessionStore(session_key)
-    session = session_wrapper.load()
-    user_id = session.get(SESSION_KEY)
-    backend_id = session.get(BACKEND_SESSION_KEY)
-    if user_id and backend_id:
-        auth_backend = load_backend(backend_id)
-        user = auth_backend.get_user(user_id)
-        if user:
-            return user
-    return AnonymousUser()
-    
-def get_or_create_userbox(user, modname):
-    try:
-        box = BillboardUserModuleBox.objects.get(user=user, modname=modname)
-    except ObjectDoesNotExist:
-        box = BillboardUserModuleBox(user=user, modname=modname, posx=-1, posy=-1)
-        box.save()
-    return box
 
 def get_or_create_bbapp(user, modcol, modweight, modname):
     try:
